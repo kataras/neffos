c2363a46359f3eb0cb59630678919311587b382c
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2019-07-07 08:38:18 +0300

    See releases for v0.0.4
    doc.go: give just the links and remove the wrong version info (that version was synchronized with its neffos.js module accidentally)

--- doc.go
+++ doc.go
@@ -1,23 +1,10 @@
-/*
+/* Package neffos is a cross-platform real-time framework with expressive, elegant API written for Gophers.
+
 Source code and other details for the project are available at GitHub:
    https://github.com/kataras/neffos
 
-Current Version
-	0.1.6
-
-Installation
-
-The only requirement is the Go Programming Language
-	$ go get -u github.com/kataras/neffos
-
-Go Client
-	Built'n with this package.
-	Types like `Conn`, `NSConn`, `Room` and `ConnHandler[Events, Namespaces, WithTimeout]` are used by both sides(`New` for server, `Dial` for client).
-	The neffos package is "hybrid/isomorphic", same code can be used for both server-side and client-side connections.
-
-Typescript/Javascript Client
-	The client package lives on its own repository for front-end developers: <https://github.com/kataras/neffos.js>.
-	The neffos.js client can run through any modern **browser** and **nodejs**.
+Wiki
+   https://github.com/kataras/neffos/wiki
 
 Examples
    https://github.com/kataras/neffos/tree/master/_examples
