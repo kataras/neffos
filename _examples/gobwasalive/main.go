c7de9ed399e8c909f5bf175ed3cc6a10bc40203d
roytan883 <blueness883@163.com>
2019-07-15 13:13:06 +0800

    optimize timingwheel CPU and memory usage

--- _examples/gobwasalive/main.go
+++ _examples/gobwasalive/main.go
@@ -1,125 +1,126 @@
 package main
 
 import (
-    "context"
-    "log"
-    "net/http"
-    "os"
-    "os/signal"
-    "strconv"
-    "syscall"
-    "time"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
 
-    "github.com/kataras/neffos"
-    "github.com/kataras/neffos/gobwasalive"
+	"github.com/kataras/neffos"
+	"github.com/kataras/neffos/gobwasalive"
 )
 
 var events = neffos.WithTimeout{
-    ReadTimeout:  20 * time.Second,
-    WriteTimeout: 20 * time.Second,
+	ReadTimeout:  60 * time.Second,
+	WriteTimeout: 60 * time.Second,
 
-    Namespaces: neffos.Namespaces{
-        "v1": neffos.Events{
-            "echo": onEcho,
-        },
-    },
+	Namespaces: neffos.Namespaces{
+		"v1": neffos.Events{
+			"echo": onEcho,
+		},
+	},
 }
 
 func onEcho(c *neffos.NSConn, msg neffos.Message) error {
-    body := string(msg.Body)
-    log.Println(body)
+	body := string(msg.Body)
+	log.Println(body)
 
-    if !c.Conn.IsClient() {
-        newBody := append([]byte("echo back: "), msg.Body...)
-        return neffos.Reply(newBody)
-    }
+	if !c.Conn.IsClient() {
+		newBody := append([]byte("echo back: "), msg.Body...)
+		return neffos.Reply(newBody)
+	}
 
-    return nil
+	return nil
 }
 
 func main() {
-    args := os.Args[1:]
-    if len(args) == 0 {
-        log.Fatalf("expected program to start with 'server' or 'client' argument")
-    }
-    side := args[0]
+	args := os.Args[1:]
+	if len(args) == 0 {
+		log.Fatalf("expected program to start with 'server' or 'client' argument")
+	}
+	side := args[0]
 
-    clientNum := 0
-    if len(args) == 2 {
-        clientNum, _ = strconv.Atoi(args[1])
-        log.Printf("Start clientNum: %v", clientNum)
-    }
+	clientNum := 0
+	if len(args) == 2 {
+		clientNum, _ = strconv.Atoi(args[1])
+		log.Printf("Start clientNum: %v", clientNum)
+	}
 
-    switch side {
-    case "server":
-        runServer()
-    case "client":
-        if clientNum == 0 {
-            runClient()
-        } else {
-            runClientMany(clientNum)
-        }
+	switch side {
+	case "server":
+		runServer()
+	case "client":
+		if clientNum == 0 {
+			runClient()
+		} else {
+			runClientMany(clientNum)
+		}
 
-    default:
-        log.Fatalf("unexpected argument, expected 'server' or 'client' but got '%s'", side)
-    }
+	default:
+		log.Fatalf("unexpected argument, expected 'server' or 'client' but got '%s'", side)
+	}
 }
 
 func runServer() {
-    upgrader := gobwasalive.NewUpgrader(16 * time.Second)
-    websocketServer := neffos.New(upgrader, events)
+	upgrader := gobwasalive.NewUpgrader(50 * time.Second)
+	websocketServer := neffos.New(upgrader, events)
 
-    websocketServer.OnConnect = func(c *neffos.Conn) error {
-        log.Printf("OnConnect cid: %v", c.ID())
-        return nil
-    }
+	websocketServer.OnConnect = func(c *neffos.Conn) error {
+		log.Printf("OnConnect cid: %v", c.ID())
+		return nil
+	}
 
-    websocketServer.OnDisconnect = func(c *neffos.Conn) {
-        log.Printf("OnDisconnect cid: %v", c.ID())
-    }
+	websocketServer.OnDisconnect = func(c *neffos.Conn) {
+		log.Printf("OnDisconnect cid: %v", c.ID())
+	}
 
-    router := http.NewServeMux()
-    router.Handle("/echo", websocketServer)
+	router := http.NewServeMux()
+	router.Handle("/echo", websocketServer)
 
-    ticker := time.NewTicker(5 * time.Second)
-    go func() {
-        for {
-            select {
-            case <-ticker.C:
-                log.Printf("total clients: %v", websocketServer.GetTotalConnections())
-            }
-        }
-    }()
+	ticker := time.NewTicker(30 * time.Second)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				log.Printf("total clients: %v", websocketServer.GetTotalConnections())
+			}
+		}
+	}()
 
-    log.Println("Serving websockets on localhost:8080/echo")
-    log.Fatal(http.ListenAndServe(":8080", router))
+	log.Println("Serving websockets on localhost:8080/echo")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func runClient() {
-    ctx := context.Background()
-    dialer := gobwasalive.NewDialer(15 * time.Second)
-    client, err := neffos.Dial(ctx, dialer, "ws://localhost:8080/echo", events)
-    if err != nil {
-        panic(err)
-    }
+	ctx := context.Background()
+	dialer := gobwasalive.NewDialer(45 * time.Second)
+	client, err := neffos.Dial(ctx, dialer, "ws://localhost:8080/echo", events)
+	if err != nil {
+		panic(err)
+	}
 
-    _, err = client.Connect(ctx, "v1")
-    if err != nil {
-        panic(err)
-    }
+	_, err = client.Connect(ctx, "v1")
+	if err != nil {
+		panic(err)
+	}
 
-    //c.Emit("echo", []byte("Greetings!"))
+	//c.Emit("echo", []byte("Greetings!"))
 
-    // a channel that blocks until client is terminated,
-    // i.e by CTRL/CMD +C.
-    <-client.NotifyClose
+	// a channel that blocks until client is terminated,
+	// i.e by CTRL/CMD +C.
+	<-client.NotifyClose
 }
 
 func runClientMany(clientNum int) {
-    for i := 0; i < clientNum; i++ {
-        go runClient()
-    }
-    ch := make(chan os.Signal)
-    signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-    log.Println(<-ch)
+	for i := 0; i < clientNum; i++ {
+		time.Sleep(5 * time.Millisecond)
+		go runClient()
+	}
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-ch)
 }
